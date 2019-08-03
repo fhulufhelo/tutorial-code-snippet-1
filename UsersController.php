8b253718c3cbfb026fcc0ad6af3b53f1449413cc
Fhulufhelo Mokhomi <mokhomifhulufhelo8@gmail.com>
2019-08-03 15:40:03 +0200

    initial commit

---
+++ UsersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UsersController extends Controller
+{
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    	if ( $request->role == 'seller') {
+
+    		# proceed this way
+
+    	} elseif ($request->role == 'buyer') {
+
+    		# proceed this another
+    	} else {
+
+    		# Do this
+    	}
+    
+    }
+
+
+}
